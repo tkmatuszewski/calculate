7de383a64afd0ac6aad2264500ae67ad7fde2160
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-02-18 22:57:50 +0100

    Firebase config

---
+++ src/components/Firebase/Firebase.js
@@ -0,0 +1,25 @@
+import app from 'firebase/app';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBdw4q2mSOlyBQIqgmeYehKB-fEI_JjJLU",
+    authDomain: "calculator-a06e2.firebaseapp.com",
+    databaseURL: "https://calculator-a06e2.firebaseio.com",
+    projectId: "calculator-a06e2",
+    storageBucket: "calculator-a06e2.appspot.com",
+    messagingSenderId: "57725969282",
+    appId: "1:57725969282:web:2e036be8875bfa5fa3a59b",
+    measurementId: "G-2L2LVEWQZ2"
+};
+
+// Initialize Firebase
+export class Firebase {
+    constructor() {
+        app.initializeApp(firebaseConfig);
+    }
+}
+
+export const data = app.firestore();
+
+export default Firebase
