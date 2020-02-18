334c8d85d739eeeb905467a3a3339ca3573c80cb
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-02-19 00:10:14 +0100

    header and footer components

---
+++ src/components/AppFooter/AppFooter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function AppFooter () {
+    return (
+        <div className={"appFooter"}> tkmatuszewski &copy;2020</div>
+    )
+}
+
+export default AppFooter
---
+++ src/components/AppHeader/AppHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AppHeader() {
+    return (
+        <div className={"appHeader"}>
+            <div className={"appHeaderContainer"}>
+                <h1 className={"appLogo"}>Calculte</h1>
+            </div>
+        </div>
+    )
+}
+
+export default AppHeader
