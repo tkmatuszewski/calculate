f3ac05c3841f9b4b989588e9bfa5e389596f7526
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-02-22 23:17:04 +0100

    CalendarEvents component

---
+++ src/components/CalendarEvents/CalendarEvents.js
@@ -0,0 +1,32 @@
+import React from "react";
+import CalendarDate from "../CalendarDate/CalendarDate";
+
+class CalendarEvents extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            key: 0
+        }
+    };
+
+    onAdded = () => {
+        this.setState({
+            key: Math.random()
+        });
+    };
+
+    render() {
+        return (
+            <div className={"calendarEvents"}>
+                <div className={"calendarEventsCnt"}>
+                    <CalendarDate date={this.props.date}/>
+                    {/*<AddEvent date={this.props.date} onAdded={this.onAdded}*/}
+                    {/*          tileContent={this.tileContent}/>*/}
+                    {/*<CalendarEventList date={this.props.date} key={this.state.key}/>*/}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CalendarEvents
