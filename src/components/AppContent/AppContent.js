f4e96b1c868f4187489d382bb4143bc6bba86342
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-02-22 23:07:17 +0100

    styling updated to match component restructurisation after Loader component addon

---
+++ src/components/AppContent/AppContent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CalendarToggle from "../CalendarToggle/CalendarToggle";
+
+function AppContent() {
+    return (
+        <div className={"appContent"}>
+            <CalendarToggle/>
+            {/*<UserPanel/>*/}
+        </div>
+    )
+}
+
+
+export default AppContent
