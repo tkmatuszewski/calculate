7d805de9461862827f6ac5b82e3e97d6f7284374
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-03-02 00:19:26 +0100

    UserPart component

--- src/components/AppContent/AppContent.js
+++ src/components/AppContent/AppContent.js
@@ -1,11 +1,12 @@
 import React from "react";
 import CalendarToggle from "../CalendarToggle/CalendarToggle";
+import UserPanel from "../UserPart/UserPart";
 
 function AppContent() {
     return (
         <div className={"appContent"}>
             <CalendarToggle/>
-            {/*<UserPanel/>*/}
+            <UserPanel/>
         </div>
     )
 }
