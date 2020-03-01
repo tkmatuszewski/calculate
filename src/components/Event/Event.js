e1bc49f5f6859e83eab90d1e23c7700a2f78229c
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-03-01 01:05:15 +0100

    EventList component added

---
+++ src/components/Event/Event.js
@@ -0,0 +1,37 @@
+import React from "react";
+import data from "../Firebase/Firebase";
+
+class Event extends React.Component {
+    state = {
+        shortInMin: "",
+        shortInPls: ""
+    };
+    eventDelete = (e) => {
+        const eventId = e.target.parentElement.getAttribute("data-id");
+        data.collection(`sub`).doc(eventId).delete();
+        e.target.parentElement.remove();
+        this.render()
+    };
+    // shortenUsers  =()=> {
+    //     let name = this.props.event.data();
+    //     let shortinMin = name.inMinus.split("");
+    //     // shortMin = shortinMin[0][0]+shortinMin[1][0]
+    //     console.log(shortinMin);
+    //     return shortinMin
+    // ;
+    render() {
+        let event = this.props.event;
+        return (
+            <li key={event} className={"eventEl"}>
+                <div data-id={event.id} className={"eventContainer"}>
+                    <div className={"eventUser"}>{event.data().inMinus}</div>
+                    <div className={"eventCount"}>{event.data().count}</div>
+                    <div className={"eventUser"}>{event.data().inPlus}</div>
+                    <button onClick={this.eventDelete} className={"eventDel"}/>
+                </div>
+            </li>
+        )
+    }
+}
+
+export default Event
