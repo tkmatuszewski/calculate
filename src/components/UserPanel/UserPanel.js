9b101797c98c97f8fb963c4655d36200459f3d61
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-03-13 00:12:43 +0100

    component restructure

--- src/components/UserPanel/UserPanel.js
+++ src/components/UserPanel/UserPanel.js
@@ -4,19 +4,13 @@ import UserAdd from "../UserAdd/UserAdd";
 import ArchiveEvents from "../ArchiveEvents/ArchiveEvents";
 
 class UserPanel extends React.Component {
-    state = {
-        businessDays: 0
-    };
-    passBusinessDays = (days) => {
-        this.setState({businessDays: days})
-    };
 
     render() {
         return (
             <div className={"userPanel"}>
-                <BusinessDays update={this.passBusinessDays}/>
-                <UserAdd businessDays={this.state.businessDays}/>
-                <ArchiveEvents />
+                <BusinessDays update={this.props.update}/>
+                <UserAdd/>
+                <ArchiveEvents/>
             </div>
         )
     }
