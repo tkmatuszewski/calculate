026de59de5a6046c022271db7e0bc2c628ffd755
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-03-13 00:13:21 +0100

    component restructure

--- src/components/UserPart/UserPart.js
+++ src/components/UserPart/UserPart.js
@@ -2,13 +2,21 @@ import React from "react";
 import UserPanel from "../UserPanel/UserPanel";
 import UserList from "../UserList/UserList";
 
-function UserPart () {
-    return (
-        <div className={"userPart"}>
-            <UserPanel/>
-            <UserList />
-        </div>
-    )
+class UserPart extends React.Component {
+    state = {
+      businessDays : 0
+    };
+    update = (days) => {
+        this.setState({businessDays: days})
+    };
+    render() {
+        return (
+            <div className={"userPart"}>
+                <UserPanel update={this.update}/>
+                <UserList businessDays = {this.state.businessDays}/>
+            </div>
+        )
+    }
 }
 
 export default UserPart
