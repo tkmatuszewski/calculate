db4d627d777d19930be5b6e81700ec3e0d465e3e
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-03-18 21:40:35 +0100

    UserMenu component added

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -8,28 +8,31 @@ class UserMenu extends React.Component {
     show = () => {
         this.setState({show: !this.state.show})
     };
-    delete = () => {
-        // let id = this.props.id;
-        // data.collection(`users`).doc(id).delete();
-        console.log(this.props.id)
-    };
+    edit = () => {
 
+    };
+    delete = () => {
+        let id = this.props.id;
+        data.collection(`users`).doc(id).delete();
+    };
     render() {
         if (this.state.show) {
             return (
-                <ul className={"userMenu"} onClick={this.show}>
-                    <li className={"userMenuElement"}>
-                        <div className={"userMenuEdit"}/>
-                        <span>Edytuj</span>
-                    </li>
-                    <li className={"userMenuElement"}>
-                        <div className={"userMenuDel"} onClick={this.delete()}/>
-                        <span>Usuń</span>
-                    </li>
-                </ul>
+                <div className={"userMenu"} onClick={this.show}>
+                    <ul className={"userMenuList"}>
+                        <li className={"userMenuElement"}>
+                            <div className={"userMenuEdit"}/>
+                            <span>Edytuj</span>
+                        </li>
+                        <li className={"userMenuElement"}>
+                            <div className={"userMenuDel"} onClick={this.delete()}/>
+                            <span>Usuń</span>
+                        </li>
+                    </ul>
+                </div>
             )
         } else {
-            return <ul className={"userMenu"} onClick={this.show}/>
+            return <div className={"userMenu"} onClick={this.show}/>
         }
     }
 }
