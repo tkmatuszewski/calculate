1e85c8a0fb624b6f2e0279b2d69d62c63623bb49
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-03-26 23:36:22 +0100

    User verified background change

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -4,11 +4,14 @@ import UserMenu from "../UserMenu/UserMenu";
 import TotalTime from "../UserTotalTime/UserTotalTime";
 import UserEvents from "../UserEvents/UserEvents";
 
+const classNames = require('classnames');
+
 class User extends React.Component {
     _isMounted = false;
     state = {
         events: [],
-        bonusHours: 0
+        bonusHours: 0,
+        verified: false
     };
     additionalCount = () => {
         const user = this.props.user.data();
@@ -24,23 +27,39 @@ class User extends React.Component {
         this.setState({bonusHours: Number(counter)});
     };
 
+    passVerification = (state) => {
+        this.setState({verified: state})
+    };
+
     render() {
+        let user = classNames({
+            user: true,
+            'verified': this.state.verified
+        });
         return (
             <>
-                <li className={"user"}>
-                    <div className={"userContainer"}>
-                        <div className={"userNames"}>
-                            <div className={"userName"}>{this.props.user.data().name}</div>
-                            <div className={"userSurname"}>{this.props.user.data().surname}</div>
-                            <div className={"userDaily"}>{this.props.user.data().dailyTime}h</div>
+                <li className={user}>
+                    <div className={"userCnt"}>
+                        <div className={"userCntTop"}>
+                            <div className={"userNames"}>
+                                <div className={"userName"}>{this.props.user.data().name}</div>
+                                <div className={"userSurname"}>{this.props.user.data().surname}</div>
+                                <div className={"userDaily"}>{this.props.user.data().dailyTime}h</div>
+                            </div>
+                            <UserMenu
+                                id={this.props.user.id}
+                                passVerification={this.passVerification}/>
                         </div>
-                        <UserMenu id={this.props.user.id}/>
-                    </div>
-                    <div className={"userLowerContainer"}>
-                        <UserEvents events={this.state.events} user={this.props.user.data()}/>
-                        <TotalTime businessDays={this.props.businessDays}
-                                   dailyTime={this.props.user.data().dailyTime}
-                                   bonusHours={this.state.bonusHours}/>
+                        <div className={"userCntBottom"}>
+                            <div className={"userCntBottomEvents"}>
+                                <UserEvents
+                                    events={this.state.events}
+                                    user={this.props.user.data()}/>
+                            </div>
+                            <TotalTime businessDays={this.props.businessDays}
+                                       dailyTime={this.props.user.data().dailyTime}
+                                       bonusHours={this.state.bonusHours}/>
+                        </div>
                     </div>
                 </li>
             </>
@@ -48,20 +67,38 @@ class User extends React.Component {
     }
 
     componentDidMount() {
-        this._isMounted = true;
-        data.collection(`sub`).get().then((el) => {
-                el.docs.map((doc) => {
-                    return this.setState({events: this.state.events.concat(doc.data())}, () => {
-                        this.additionalCount();
-                    })
-                });
-            }
-        )
-    }
+        // this._isMounted = true;
+        // data.collection(`sub`).get().then((el) => {
+        //         el.docs.map((doc) => {
+        //             return this.setState({events: this.state.events.concat(doc.data())}, () => {
+        //                 this.additionalCount();
+        //             })
+        //         });
+        //     }
+        // )
+        data.collection(`sub`).onSnapshot((querySnapshot) => {
+            querySnapshot.docChanges().map((change) => {
+                if (change.type === "added") {
+                    return this.setState({
+                        events: this.state.events.concat(change.doc),
+                    });
+                }
+                if (change.type === "modified") {
+                    return console.log("Zmodyfikowano wydarzenie: ", change.doc.data());
+                }
+                if (change.type === "removed") {
+                    const filtered = this.state.events.filter(event => event.id !== change.doc.id);
+                    return this.setState({events: filtered});
+                }
+            })
+        }).error = (error) => {
+            return console.log(error)
+        };
+    };
 
-    componentWillUnmount() {
-        this._isMounted = false;
-    }
+    // componentWillUnmount() {
+    //     this._isMounted = false;
+    // }
 }
 
 export default User
