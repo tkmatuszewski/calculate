f47af76773579488b41dc74f5c146c871fa26f40
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-03-26 23:21:49 +0100

    BusinessDaysForm redesigned and optimised

--- src/components/BusinessDaysForm/BusinessDaysForm.js
+++ src/components/BusinessDaysForm/BusinessDaysForm.js
@@ -3,37 +3,51 @@ import data from "../Firebase/Firebase";
 
 class BusinessDaysForm extends React.Component {
     state = {
-        businessDays: "",
-        show: true
-};
-inputHandler = (e) => {
-    this.setState({[e.target.name]: e.target.value});
-};
-submitHandler = (e) => {
-    e.preventDefault();
-    this.props.update(this.state.businessDays);
-    data.collection(`businessDays`).doc("hvcziTCipJEMkNgYIxRt").set(this.state);
-    this.props.toggleForm();
-};
+        businessDays: this.props.businessDays,
+        show: true,
+        message: ""
+    };
+    inputHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+    };
+    submitHandler = (e) => {
+        e.preventDefault();
+        this.props.update(this.state.businessDays);
+        data.collection(`businessDays`).doc("hvcziTCipJEMkNgYIxRt").set(this.state);
+        this.setState({message: "Zaktualizowano liczbę dni roboczych"});
 
-render()
-{
-    return (
-        <form className={"businessDaysForm"} onSubmit={this.submitHandler}>
-            <div className={"businessDaysFormTop"}>
-                <h3 className={"businessDaysFormTitle"}>Liczba dni pracujących w miesiącu</h3>
-                <button className={"businessDaysFormClose"} onClick={this.closeForm}/>
+        setTimeout(() => {
+            this.props.passToggleForm(false);
+            return this.setState({message: ""})
+        }, 3000);
+    };
+
+    closeForm = () => {
+        this.setState({show: false});
+        this.props.passToggleForm(false);
+    };
+
+    render() {
+        return (
+            <div className={"businessDaysFormMask"}>
+                <form className={"businessDaysForm"} onSubmit={this.submitHandler}>
+                    <div className={"businessDaysFormTop"}>
+                        <button className={"businessDaysFormClose"} type="button" onClick={this.closeForm}/>
+                    </div>
+                    <h3 className={"businessDaysFormTitle"}>Dni robocze</h3>
+                    <div className={"businessDaysFormMsg"}>{this.state.message}</div>
+                    <div className={"businessDaysFormBottom"}>
+                        <label className={"businessDaysFormLabel"}>Podaj liczbę dni roboczych w miesiącu
+                            <input type="number" name="businessDays" className={"businessDaysFormInput"}
+                                   onChange={this.inputHandler} placeholder={this.state.businessDays}/>
+                        </label>
+                        <button className={"businessDaysFormBtn"} type={"submit"}>Zatwierdź
+                        </button>
+                    </div>
+                </form>
             </div>
-            <div className={"businessDaysFormBottom"}>
-                <input type="number" name="businessDays" className={"businessDaysFormInput"}
-                       onChange={this.inputHandler} placeholder={this.state.businessDays}/>
-                <button className={"businessDaysFormBtn"}>
-                    <div className={"businessDaysFormBtnIcon"}/>
-                </button>
-            </div>
-        </form>
-    )
-}
+        )
+    }
 }
 
 export default BusinessDaysForm
