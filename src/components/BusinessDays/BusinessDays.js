d54e56b32ea039df09e4ffbcc2bee3777d6b496a
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-04-14 13:21:52 +0200

    contidional rendering of form, mobile breakpoints

--- src/components/BusinessDays/BusinessDays.js
+++ src/components/BusinessDays/BusinessDays.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, {Component} from "react";
 import BusinessDaysForm from "../BusinessDaysForm/BusinessDaysForm";
 import data from "../Firebase/Firebase";
 
-class BusinessDays extends React.Component {
+class BusinessDays extends Component {
     state = {
         show: false,
-        businessDays: 0
+        businessDays: 0,
+        showTileDescription: false
     };
 
     toggleForm = () => {
@@ -13,44 +14,45 @@ class BusinessDays extends React.Component {
     };
 
     passToggleForm = (state) => {
-        this.setState({show : state})
+        this.setState({show: state})
     };
 
-    update = (state) => {
+    updateBusinessDaysOnChange = (state) => {
         this.setState({businessDays: state})
     };
-    renderTile = () => {
-        return (
-            <div className={"businessDays"} onClick={this.toggleForm}>
-                <div className={"businessDaysIcon"}>
-                    <div className={"businessDaysVal"}>{this.state.businessDays}</div>
-                </div>
-                <div className={"businessDaysDsc"}>Dni robocze</div>
-            </div>
-        )
+    handleMouseover = () => {
+        this.setState({showTileDescription: !this.state.showTileDescription})
     };
 
     render() {
-        if (this.state.show) {
-            return (
-                <>
-                    {this.renderTile()}
-                    <BusinessDaysForm
-                        passToggleForm = {this.passToggleForm}
-                        update={this.update}
-                        businessDays={this.state.businessDays}/>
-                </>
-            )
-        } else {
-           return this.renderTile()
-        }
+        return (
+            <>
+                <div className={"businessDays"}
+                     onClick={this.toggleForm}
+                     onMouseEnter={this.handleMouseover}
+                     onMouseLeave={this.handleMouseover}>
+                    <div className={"businessDaysIcon"}>
+                        <div className={"businessDaysVal"}>{this.state.businessDays}</div>
+                    </div>
+                    {this.state.showTileDescription && <div className={"businessDaysDsc"}>Dni robocze</div>}
+                    <span className={"userAddMobile"}>Dni robocze
+                    <span className={"businessDaysDecor"}/>
+                </span>
+                </div>
+                {this.state.show && <BusinessDaysForm
+                    passToggleForm={this.passToggleForm}
+                    updateBusinessDaysOnChange={this.updateBusinessDaysOnChange}
+                    businessDaysToUserPart={this.props.businessDaysToUserPart}
+                    businessDays={this.state.businessDays}/>}
+            </>
+        )
     }
 
     componentDidMount() {
         data.collection(`businessDays`).doc("hvcziTCipJEMkNgYIxRt").get()
             .then((doc) => {
                 this.setState({businessDays: doc.data().businessDays});
-                this.props.update(this.state.businessDays);
+                this.props.businessDaysToUserPart(this.state.businessDays)
             })
             .catch((error => {
                 console.log(error)
