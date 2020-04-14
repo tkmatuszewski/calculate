25edc2cbc3ac7a5621ff6c5e78574dfd11c38e4b
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-04-14 15:20:14 +0200

    UserList bug fixes

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,48 +1,62 @@
-import React from "react";
+import React, {Component} from "react";
 import User from "../User/User";
 import data from "../Firebase/Firebase";
 
-class UserList extends React.Component {
+class UserList extends Component {
+    _isMounted = false;
     state = {
         users: [],
     };
+
     renderUsers = () => {
-        return this.state.users.map((el) => {
-            return <User user={el} businessDays={this.props.businessDays} key={el.id}/>
+        return this.state.users.map((user) => {
+            return <User
+                user={user}
+                businessDays={this.props.businessDays}
+                events={this.props.events}
+                key={user.id}/>
         })
     };
 
     render() {
         return (
             <>
-                <ul className={"userList"}>
+                {!this.props.archiveMode && <ul className={"userList"}>
+                    {this.state.users.length > 0 && <li className="userListSide">Pracownicy</li>}
                     {this.renderUsers()}
-                </ul>
+                </ul>}
             </>
         )
     }
 
     componentDidMount() {
+        this._isMounted = true;
 
         data.collection(`users`).onSnapshot((querySnapshot) => {
             querySnapshot.docChanges().map((change) => {
+                let userData = "";
                 if (change.type === "added") {
-                    return this.setState({
+                    userData = this.setState({
                         users: this.state.users.concat(change.doc),
                     });
                 }
                 if (change.type === "modified") {
-                    return console.log("Zmodyfikowano pracownika: ", change.doc.data());
+                    return console.log("Zmodyfikowano pracownika: ", change.doc);
                 }
                 if (change.type === "removed") {
                     const filtered = this.state.users.filter(user => user.id !== change.doc.id);
-                    return this.setState({users: filtered});
+                    userData = this.setState({users: filtered});
                 }
+                return userData;
             })
         }).error = (error) => {
             return console.log(error)
         };
     };
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
 }
 
 export default UserList
