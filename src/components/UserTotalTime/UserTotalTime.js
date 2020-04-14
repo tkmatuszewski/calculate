f23eae62dffc00407df267aa368c687bd43b12cd
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-04-14 15:29:50 +0200

    recalculation of props change

--- src/components/UserTotalTime/UserTotalTime.js
+++ src/components/UserTotalTime/UserTotalTime.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, {Component} from "react";
 
-function TotalTime (props) {
-    let totalTime = props.businessDays * props.dailyTime + props.bonusHours;
-    return <div className={"totalTime"}> {totalTime} </div>
+class TotalTime extends Component {
+
+    state = {
+        totalTime: ""
+    };
+
+    render() {
+
+        const totalTime = (this.props.businessDays * this.props.dailyTime) + this.props.bonusHours;
+
+        return <div className={"totalTime"}> {totalTime}
+            <span>h</span>
+        </div>
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.businessDays !== prevProps.businessDays) {
+            this.setState({reloadOnPropsChange: Math.random()})
+        }
+    }
 }
 
 export default TotalTime
