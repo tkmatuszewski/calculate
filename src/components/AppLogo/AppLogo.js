344b2daf0548ba5852ad240f39eb22374b8e7a12
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-04-22 01:08:02 +0200

    AppLogo component

---
+++ src/components/AppLogo/AppLogo.js
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+const classNames = require('classnames');
+
+const AppLogo = ()=> {
+
+    const [activeLogo, setActiveLogo] = useState(false);
+
+    const handleMouseOver = () => {
+        return setActiveLogo(!activeLogo)
+    };
+
+    const logoBackground = classNames({
+        "appLogoBg": true,
+        active: activeLogo
+    });
+    return (
+        <div className={"appLogo"}
+             onMouseEnter={handleMouseOver}
+             onMouseLeave={handleMouseOver}>
+            <Link to={"/"}>Calculate</Link>
+            <div className={logoBackground}/>
+        </div>
+    )
+};
+
+export default AppLogo
