d03537501bde29f9c582bcad2ae85f2bf09fa7a6
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-04-22 01:41:55 +0200

    minor code clearence

--- src/components/Firebase/FirebaseAuth.js
+++ src/components/Firebase/FirebaseAuth.js
@@ -5,6 +5,7 @@ export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
+
     useEffect(()=> {
         app.auth().onAuthStateChanged(setCurrentUser)
     }, []);
