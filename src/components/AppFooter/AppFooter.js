118c10eb4b96a7ad845a4ab4f4831db98a64df29
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-04-27 17:09:32 +0200

    AppFooter code readibility improved

--- src/components/AppFooter/AppFooter.js
+++ src/components/AppFooter/AppFooter.js
@@ -1,9 +1,12 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-function AppFooter () {
+function AppFooter() {
     return (
-        <div className={"appFooter"}> <span>tkmatuszewski &copy;2020</span> <Link to={"/contributions"}>Podziękowania</Link></div>
+        <div className={"appFooter"}>
+            <span>tkmatuszewski &copy;2020</span>
+            <Link to={"/contributions"}>Podziękowania</Link>
+        </div>
     )
 }
 
