cbe0123f25ebc680132ca7cbbca19c0b1c7ae862
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-04-27 22:53:20 +0200

    CalendarToggle classname rename and minor code opt

--- src/components/CalendarToggle/CalendarToggle.js
+++ src/components/CalendarToggle/CalendarToggle.js
@@ -2,12 +2,9 @@ import React, {Component} from "react";
 import classNames from 'classnames';
 
 class CalendarToggle extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+        state = {
             showCalendar: true,
-        }
-    };
+        };
 
     toggleCalendar = () => {
         this.setState({showCalendar: !this.state.showCalendar},
@@ -18,13 +15,13 @@ class CalendarToggle extends Component {
     };
 
     render() {
-        const calendarToggler = classNames({
+        const calendarToggle = classNames({
             calendarToggleIcon: true,
             calendarMode: this.state.showCalendar
         });
         return (
             <div className={"calendarToggle"} onClick={this.toggleCalendar}>
-                <div className={calendarToggler}/>
+                <div className={calendarToggle}/>
             </div>
         )
     }
